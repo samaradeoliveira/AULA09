54e1059e0ce591bde690f6b27a5f11d73ee26860
Oliver <95024811+samaradeoliveira@users.noreply.github.com>
2023-04-18 19:39:59 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+//criar uma var que será uma caixa
+
+
+function setup(){
+    //área do jogo
+    createCanvas(400, 400);
+
+    //criar uma sprite nesta var
+
+}
+
+function draw(){
+    background("purple");
+
+    //condições para mover a caixa
+
+
+
+
+    drawSprites();
+}
